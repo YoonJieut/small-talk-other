746d5ffbc1b696c0fc519354cba7ba4cf75f081a
YoonJunHyeon <qmdldos@gmail.com>
2023-10-25 15:28:18 +0900

    바르게 구동 확인, 기능 추가 스타트!!!

--- public/util/script.js
+++ public/util/script.js
@@ -1,25 +1,29 @@
-import { messages } from "../scriptData/data.js";
-import { messages1_1 } from "../scriptData/data1-1.js";
-import { messages1_2 } from "../scriptData/data1-2.js";
-import { messages1_3 } from "../scriptData/data1-3.js";
-import { messages2 } from "../scriptData/data2.js";
-import { messages2_1 } from "../scriptData/data2-1.js";
+import { messages } from '../scriptData/data.js';
 
-const container = document.querySelector('.container');
+const container = document.querySelector(".container");
 
 messages.forEach(message => {
-  const messageDiv = document.createElement('div');
-  messageDiv.classList.add('message', message.type);
+    const messageDiv = document.createElement('div');
+    messageDiv.classList.add('message', message.type);
 
-  const profileDiv = document.createElement('div');
-  profileDiv.classList.add('profile');
-  profileDiv.innerText = message.author[0];  // 첫 글자만
+    if (message.type === 'narration') {
+        // "해설" 텍스트의 경우
+        const narrationText = document.createElement('div');
+        narrationText.classList.add('narration');
+        narrationText.innerText = message.text;
+        messageDiv.appendChild(narrationText);
+    } else {
+        const profileDiv = document.createElement('div');
+        profileDiv.classList.add('profile');
+        profileDiv.innerText = message.author[0];  // 첫 글자만
 
-  const textBox = document.createElement('div');
-  textBox.classList.add('text-box');
-  textBox.innerText = message.text;
+        const textBox = document.createElement('div');
+        textBox.classList.add('text-box');
+        textBox.innerText = message.text;
 
-  messageDiv.appendChild(profileDiv);
-  messageDiv.appendChild(textBox);
-  container.appendChild(messageDiv);
+        messageDiv.appendChild(profileDiv);
+        messageDiv.appendChild(textBox);
+    }
+
+    container.appendChild(messageDiv);
 });
